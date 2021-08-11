0f96ac8fca7f3ea8e42461cc55eb61709b1829ca
SyHoMadara <79520000+SyHoMadara@users.noreply.github.com>
2021-08-11 17:33:52 +0430

    action_stream created

---
+++ applications/action_stream/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ActionStreamConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'applications.action_stream'
---
+++ applications/action_stream/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+from applications.account import User
+from applications.product import Product
+from django.utils.translation import gettext_lazy as _
+
+
+class Action(models.Model):
+    description = models.CharField(max_length=400, blank=False)
+    date = models.DateTimeField(_('date action'), default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+
+    def __init__(self, user: User, product: Product, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.product = product
+        self.user = user
+        self.date = timezone.now()
